d836609193d0e23612b1a5cd0f55b0f1f490b879
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-07 10:42:05 +0200

    printalphabet 4

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	alphabet:="abcdefghijklmnopqrstuvwxyz"
-	for _,char := range alphabet {
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	for _, char := range alphabet {
 		z01.PrintRune(rune(char))
 	}
 	z01.PrintRune('\n')
