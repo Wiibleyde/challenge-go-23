e1aba20e8ea852429071cc30be73fbde076a1cbd
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-07 16:41:36 +0200

    printnbr 1

---
+++ printnbr.go
@@ -0,0 +1,41 @@
+package student
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func reverse_int(n int) int {
+    new_int := 0
+	negative := false
+	if n < 0 {
+		negative = true
+		n = n * -1
+	}
+    for n > 0 {
+        remainder := n % 10
+        new_int *= 10
+        new_int += remainder 
+        n /= 10
+	}
+	if negative == true {
+		return new_int*-1
+	} else {
+	return new_int 
+	}
+}
+
+func PrintNbr(n int) {
+	nb := reverse_int(n)
+	if nb < 0 {
+		z01.PrintRune('-')
+		nb = nb * -1
+	}
+	if nb == 0 {
+		z01.PrintRune('0')
+	}
+	for nb > 0 {
+		z01.PrintRune(rune(nb % 10 + '0'))
+		nb = nb / 10
+	}
+	z01.PrintRune('\n')
+}
