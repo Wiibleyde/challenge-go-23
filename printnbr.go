e96d120815aac68cba8324c8ed170cb5395abe2b
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-07 17:04:52 +0200

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -1,40 +1,53 @@
-package student
+package main
 
 import (
 	"github.com/01-edu/z01"
 )
 
-func reverse_int(n int) int {
-	new_int := 0
-	negative := false
-	if n < 0 {
-		negative = true
-		n = n * -1
-	}
-	for n > 0 {
-		remainder := n % 10
-		new_int *= 10
-		new_int += remainder
-		n /= 10
-	}
-	if negative {
-		return new_int * -1
-	} else {
-		return new_int
-	}
+func main() {
+	PrintNbr(-9223372036854775808)
+	z01.PrintRune('\n')
 }
 
+// func reverse_int(n int) int {
+// 	new_int := 0
+// 	negative := false
+// 	if n < 0 {
+// 		negative = true
+// 		n = n * -1
+// 	}
+// 	for n > 0 {
+// 		remainder := n % 10
+// 		new_int *= 10
+// 		new_int += remainder
+// 		n /= 10
+// 	}
+// 	if negative {
+// 		return new_int * -1
+// 	} else {
+// 		return new_int
+// 	}
+// }
+
 func PrintNbr(n int) {
-	nb := reverse_int(n)
-	if nb < 0 {
+	negative := n < 0
+	if negative {
 		z01.PrintRune('-')
-		nb = nb * -1
 	}
-	if nb == 0 {
-		z01.PrintRune('0')
-	}
-	for nb > 0 {
-		z01.PrintRune(rune(nb%10 + '0'))
-		nb = nb / 10
+
+	rec(n)
+}
+
+func rec(n int) {
+	for i := int(0); i < 10; i++ {
+		if n <= 9 && n >= -9 {
+			z01.PrintRune(rune(n + 48))
+			return
+		}
+		if (n-i)%10 == 0 {
+			rec((n-i)/10)
+			z01.PrintRune(rune(i + 48))
+			return
+		}
 	}
 }
