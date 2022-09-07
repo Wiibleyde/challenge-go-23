e496270314c1fcd3b0db465cd044092aae6871e2
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-07 17:28:56 +0200

    printnbr 8

--- printnbr.go
+++ printnbr.go
@@ -3,24 +3,27 @@ package student
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-	negative := n < 0
-	if negative {
-		z01.PrintRune('-')
-	}
+    negative := n < 0
+	if (n -1) != 0 {
+		n*=-1
+    }
+    if negative {
+        z01.PrintRune('-')
+    }
 
-	rec(n)
+    rec(n)
 }
 
 func rec(n int) {
-	for i := int(0); i <= 9; i++ {
-		if n <= 9 && n >= -9 {
-			z01.PrintRune(rune(n + 48))
-			return
-		}
-		if (n-i)%10 == 0 {
-			rec((n - i) / 10)
-			z01.PrintRune(rune(i + 48))
-			return
-		}
-	}
+    for i := int(0); i <= 9; i++ {
+        if n <= 9 && n >= -9 {
+            z01.PrintRune(rune(n + 48))
+            return
+        }
+        if (n-i)%10 == 0 {
+            rec((n - i) / 10)
+            z01.PrintRune(rune(i + 48))
+            return
+        }
+    }
 }
