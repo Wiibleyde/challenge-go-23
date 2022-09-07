a9f3165c337671a2ac97a1e22739a8e8db60954b
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-07 12:22:37 +0200

    printcomb 7

--- printcomb.go
+++ printcomb.go
@@ -15,7 +15,7 @@ func PrintComb() {
 					if i != 9 {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
-					} else if k == 9 {
+					} else if k == 9 || j == 8 || i == 7 {
 						z01.PrintRune('\n')
 					}
 				}
