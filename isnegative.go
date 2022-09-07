cbf7a54ec791af30f64feaef73387d2fba1bedd3
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-07 11:08:20 +0200

    isnegative 6

--- isnegative.go
+++ isnegative.go
@@ -1,9 +1,15 @@
-package IsNegative
+package student
 
-func IsNegative (nb int) {
+func IsNegative(nb int) {
 	if nb < 0 {
 		print(true)
 	} else {
 		print(false)
 	}
+}
+
+func main() {
+	IsNegative(1)
+	IsNegative(0)
+	IsNegative(-1)
 }
