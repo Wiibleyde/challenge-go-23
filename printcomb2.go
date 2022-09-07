6c5151cba1b7149120febcebf810075336fe0860
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-07 15:06:21 +0200

    printcomb2 7

--- printcomb2.go
+++ printcomb2.go
@@ -5,25 +5,26 @@ import (
 )
 
 func PrintComb2() {
-	for a := 0; a < 10; a++ {
-		for b := 0; b < 10; b++ {
-			for i := 0; i < 10; i++ {
-				for j := 0; j < 10; j++ {
-					if a+b < i+j {
-						z01.PrintRune(rune(a) + 48)
-						z01.PrintRune(rune(b) + 48)
-						z01.PrintRune(' ')
-						z01.PrintRune(rune(i) + 48)
-						z01.PrintRune(rune(j) + 48)
-						if rune(a+48) != '9' || rune(b+48) != '8' || rune(i+48) != '9' || rune(j+48) != '9' {
-							z01.PrintRune(',')
-							z01.PrintRune(' ')
-						} else {
-							z01.PrintRune('\n')
-						}
+	for a := 0; a <= 9; a++ {
+		for b := 0; b <= 9; b++ {
+			for i := a; i <= 9; i++ {
+				for j := 0; j <= 9; j++ {
+					if (a==i && b == j) || (i <= a && j <= b) {
+						continue
 					}
+					z01.PrintRune(rune(a) + 48)
+					z01.PrintRune(rune(b) + 48)
+					z01.PrintRune(' ')
+					z01.PrintRune(rune(i) + 48)
+					z01.PrintRune(rune(j) + 48)
+					if !(a == 9 && b == 8 && i == 9 && j == 9) {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+					
 				}
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
