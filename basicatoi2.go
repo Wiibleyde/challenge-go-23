f86d16d985dbd05f93a8179e8b65c9485cb040d5
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-08 12:01:38 +0200

    basicatoi2 1

---
+++ basicatoi2.go
@@ -0,0 +1,20 @@
+package student
+
+func BasicAtoi2(s string) int {
+	nb := 0
+	for _, ch := range s {
+		nb = nb*10 + charToInt2(ch)
+	}
+	return nb
+}
+
+func charToInt2(char rune) int {
+	count := 0
+	if char < 48 || char > 58 {
+		return 0
+	}
+	for i := '1'; i <= char; i++ {
+		count++
+	}
+	return count
+}
