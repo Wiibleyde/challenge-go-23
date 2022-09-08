099c0c9a6e6acb4ce506b870c81536abf2b90e7e
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-08 11:04:52 +0200

    strrev

--- strrev.go
+++ strrev.go
@@ -2,8 +2,8 @@ package student
 
 func StrRev(s string) string {
 	runes := []rune(s)
-    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-        runes[i], runes[j] = runes[j], runes[i]
-    }
-    return string(runes)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
