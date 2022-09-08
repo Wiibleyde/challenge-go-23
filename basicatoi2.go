c2e1b75684b618a8e0f411a916fe3efce8d94516
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-08 16:43:21 +0200

    verif

--- basicatoi2.go
+++ basicatoi2.go
@@ -2,14 +2,12 @@ package student
 
 func BasicAtoi2(s string) int {
 	nm := 0
-
 	for _, ch := range s {
 		if !containsIn0to9(ch) {
 			return 0
 		}
 		nm = nm*10 + charToInt(ch)
 	}
-
 	return nm
 }
 
