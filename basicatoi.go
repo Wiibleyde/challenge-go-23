da29d546327bbcab07d0d17c07187547f90b8369
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-08 11:48:12 +0200

    basicatoi 1

---
+++ basicatoi.go
@@ -0,0 +1,22 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	nb := 0
+	for _, ch := range s {
+		nb = nb*10 + charToInt(ch)
+	}
+	return nb
+}
+
+func charToInt(char rune) int {
+	count := 0
+	if char < 48 && char > 58 {
+		return 0
+	}
+
+	for i := '1'; i <= char; i++ {
+		count++
+	}
+
+	return count
+}
