45451de30b8291f9397588244db25bbac9c89900
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-08 14:11:20 +0200

    atoi 3

--- atoi.go
+++ atoi.go
@@ -27,35 +27,3 @@ func Atoi(s string) int {
 	}
 	return nb
 }
-
-func StrLen(s string) int {
-	counter := 0
-	for _, ele := range s {
-		_ = ele
-		counter = counter + 1
-	}
-	return counter
-}
-
-func containsIn0to9(ch rune) bool {
-	for i := '0'; i <= '9'; i++ {
-		if ch == i {
-			return true
-		}
-	}
-
-	return false
-}
-
-func charToInt(ele rune) int {
-	count := 0
-	if ele < 48 && ele > 58 {
-		return 0
-	}
-
-	for i := '1'; i <= ele; i++ {
-		count++
-	}
-
-	return count
-}
