9cdb1669cf24ea0c873384e06d29cfdc4b89edc1
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-12 10:21:27 +0200

    IterativeFactorial 2

---
+++ iterativefactorial.go
@@ -0,0 +1,12 @@
+package student
+
+func IterativeFactorial(nb int) int {
+	var fact int = 1
+	if (nb < 0) {
+		return 0
+	}
+	for i := 1; i <= nb; i++ {  
+		fact = fact * i	
+	}
+	return fact
+}
--- main/main.go
+++ main/main.go
@@ -1,13 +1,17 @@
 package main
 
-import "student"
+import (
+	"fmt"
+	"student"
+)
 
 func main () {
-	var a int
-	var b int
-	a = 1
-	b = 0
-	student.Swap(&a, &b)
-	println(a)
-	println(b)	
+	// var a int
+	// var b int
+	// a = 1
+	// b = 0
+	// student.Swap(&a, &b)
+	// println(a)
+	// println(b)	
+	fmt.Println(student.IterativeFactorial(4))
 }
