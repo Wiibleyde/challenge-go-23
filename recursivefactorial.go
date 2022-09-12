7c6ae1680b3af4268119f2245463d3d2397d39c5
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-12 10:34:54 +0200

    RecursiveFactorial 1

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -4,7 +4,7 @@ func RecursiveFactorial(nb int) int {
 	a := 0
 	if nb == 0 {
 		return 1
-	} else if nb < 0 || nb >= 13 {
+	} else if nb < 0 || nb >= 30 {
 		return 0
 	} else {
 		a = nb * RecursiveFactorial(nb-1)
