3d5d0df5ecd3baceae626e58ff62aaf90fd2d742
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-15 09:39:55 +0200

    compare

--- main/main.go
+++ main/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"student"
 
-	"github.com/01-edu/z01"
+	"fmt"
+	
+	// "github.com/01-edu/z01"
 )
 
 func main() {
-	z01.PrintRune(student.NRune("Hello!", 3))
-	z01.PrintRune(student.NRune("Salut!", 2))
-	z01.PrintRune(student.NRune("Bye!", -1))
-	z01.PrintRune(student.NRune("Bye!", 5))
-	z01.PrintRune(student.NRune("Ola!", 4))
-	z01.PrintRune('\n')
+	fmt.Println(student.Compare("Hello!", "Hello!"))
+	fmt.Println(student.Compare("Salut!", "lut!"))
+	fmt.Println(student.Compare("Ola!", "Ol"))
 }
