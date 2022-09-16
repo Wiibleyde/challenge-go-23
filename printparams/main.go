e011d608d3f823345e273984fec04daf6c634c8a
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-16 15:10:51 +0200

    .\printparams\main.go

---
+++ printparams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args
+	for _, ele := range args[1:] {
+		for _, ele2 := range ele {
+			z01.PrintRune(ele2)
+			z01.PrintRune('\n')
+		}
+	}
+	z01.PrintRune('\n')
+}
