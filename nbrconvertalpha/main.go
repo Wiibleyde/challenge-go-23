11c7d07f891db156f323a6b608a7818b792e0ab2
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-16 15:31:36 +0200

    nbconvertalpha

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+	for _, ele := range args {
+		for _, ele2 := range ele {
+			z01.PrintRune(rune(ele2))
+		}
+		z01.PrintRune('\n')
+	}
+}
