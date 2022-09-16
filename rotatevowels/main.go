afcece69b6d309f156f660f8b6d20226d3652951
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-16 18:01:46 +0200

    rotaterowels

--- rotatevowels/main.go
+++ rotatevowels/main.go
@@ -11,6 +11,7 @@ func main() {
 	args := ""
 	vowels := []string{}
 	result := ""
+	changeEffectued:=false
 	if len(arguments) < 1 {
 		z01.PrintRune('\n')
 	}
@@ -20,25 +21,32 @@ func main() {
 	for i := 0; i < len(args); i++ {
 		if args[i] == 'a' || args[i] == 'e' || args[i] == 'i' || args[i] == 'o' || args[i] == 'u' || args[i] == 'A' || args[i] == 'E' || args[i] == 'I' || args[i] == 'O' || args[i] == 'U' {
 			vowels = append(vowels, string(args[i]))
+			changeEffectued=true
 		}
 	}
 	for i := 0; i < len(vowels); i++ {
 		for j := i + 1; j < len(vowels); j++ {
 			vowels[i], vowels[j] = vowels[j], vowels[i]
+			changeEffectued=true
 		}
 	}
 	j := 0
 	for i := 0; i < len(args); i++ {
 		if args[i] == 'a' || args[i] == 'e' || args[i] == 'i' || args[i] == 'o' || args[i] == 'u' || args[i] == 'A' || args[i] == 'E' || args[i] == 'I' || args[i] == 'O' || args[i] == 'U' {
 			result = result + vowels[j]
+			changeEffectued=true
 			j++
 
 		} else {
 			result = result + string(args[i])
+			changeEffectued=true
 		}
 	}
-	z01.PrintRune('\n')
 	for i := 0; i < len(result); i++ {
 		z01.PrintRune(rune(result[i]))
+		changeEffectued=true
+	}
+	if changeEffectued {
+		z01.PrintRune('\n')
 	}
 }
