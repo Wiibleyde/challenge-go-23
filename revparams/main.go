6380b641046b614f54cc9f2bf30373c1ad6e6eef
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-16 15:15:28 +0200

    revparams

---
+++ revparams/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		args[i], args[j] = args[j], args[i]
+	}
+	for _, ele := range args[1:] {
+		for _, ele2 := range ele {
+			z01.PrintRune(ele2)
+		}
+		z01.PrintRune('\n')
+	}
+}
