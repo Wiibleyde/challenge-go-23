ef4d926cfeda1b8fbe180d005c306a8f42fea59b
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-19 15:05:04 +0200

    appendrange

--- appendrange.go
+++ appendrange.go
@@ -1,8 +1,8 @@
 package student
 
 func AppendRange(min, max int) []int {
-	finalList:=[]int(nil)
-	for i:=min;i<max;i++ {
+	finalList := []int(nil)
+	for i := min; i < max; i++ {
 		finalList = append(finalList, i)
 	}
 	return finalList
