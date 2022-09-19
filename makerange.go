ea0cf9a44416593790e5ed2760afbac761099a0f
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-19 15:12:09 +0200

    makerange

---
+++ makerange.go
@@ -0,0 +1,12 @@
+package student 
+
+func MakeRange(min, max int) []int {
+	if max-min<1 {
+		return []int(nil)
+	}
+	finalList:=make([]int, max-min)
+	for i:=0; i<len(finalList);i++ {
+		finalList[i]=i+min
+	}
+	return finalList
+}
