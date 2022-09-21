951353e20615d9f99f426bf99129719324601593
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-21 08:54:41 +0200

    SplitModif

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,18 +1,30 @@
 package student
 
-func SplitWhiteSpaces(s string) []string {
-	tab := []string{}
-	str := ""
-	for _, j := range s {
-		if j == ' ' || j == '\n' || j == '\t' {
-			if str != "" {
-				tab = append(tab, str)
-			}
-			str = ""
-		} else {
-			str = str + string(j)
+func SplitWhiteSpaces(str string) []string {
+	ind1 := 0
+	noWS := false
+	for index := range str {
+		if noWS && index != 0 && (str[index-1] == '\n' || str[index-1] == '\t' || str[index-1] == ' ') && str[index] != '\n' && str[index] != '\t' && str[index] != ' ' {
+			ind1++
+		}
+		if str[index] != '\n' && str[index] != '\t' && str[index] != ' ' {
+			noWS = true
 		}
 	}
-	tab = append(tab, str)
-	return tab
+	ind1++
+	x := 0
+	ans := make([]string, ind1)
+	ok := true
+	for _, c := range str {
+		if c == '\n' || c == '\t' || c == ' ' {
+			if !ok {
+				x++
+			}
+			ok = true
+			continue
+		}
+		ans[x] = ans[x] + string(c)
+		ok = false
+	}
+	return ans
 }
