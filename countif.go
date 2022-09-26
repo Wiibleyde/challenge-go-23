39250c0c000cc6b92dc3f0b946944a2b0f113c4e
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-26 14:39:21 +0200

    countif

--- countif.go
+++ countif.go
@@ -1,8 +1,8 @@
 package student
 
 func CountIf(f func(string) bool, tab []string) int {
-	counter:=0
-	for _,ele:=range tab {
+	counter := 0
+	for _, ele := range tab {
 		if f(ele) {
 			counter++
 		}
