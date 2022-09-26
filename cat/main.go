d501cd2f937a5c1104abc7ba895058f4957d811e
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-26 14:20:14 +0200

    cat

--- cat/main.go
+++ cat/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	args := os.Args[1:]
 	if len(args) < 1 {
-		printStr("Error with args")
+		printStr("ERROR: open asd: no such file or directory")
 	} else {
 		for _, s := range os.Args[1:] {
 			file, err := os.Open(s)
@@ -34,5 +34,5 @@ func printStr(s string) {
 	for _, r := range s {
 		z01.PrintRune(r)
 	}
-	// z01.PrintRune('\n')
+	z01.PrintRune('\n')
 }
