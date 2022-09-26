51870b175ee66f14e223192db947b1bb63f2859e
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-26 12:19:21 +0200

    displayfile

---
+++ displayfile/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) > 1 {
+		fmt.Println("Too many arguments")
+	} else if len(args) < 1 {
+		fmt.Println("File name missing")
+	} else {
+		printFile(args[0])
+	}
+}
+
+func printFile(filename string) {
+	content, _ := os.ReadFile(filename)
+	fmt.Println(string(content))
+}
