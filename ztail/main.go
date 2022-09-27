c6208b2484f692401badd060bd8d32ad0ff1c309
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-27 09:04:23 +0200

    ztail test

--- ztail/main.go
+++ ztail/main.go
@@ -21,48 +21,37 @@ func numberOfBytes(args []string) (int, []string) {
 				os.Exit(1)
 			}
 			arg := args[i+1]
-
 			nbytes, err = strconv.Atoi(arg)
-
 			if err != nil {
 				fmt.Printf("tail: invalid number of bytes: %s\n", arg)
 				os.Exit(1)
 			}
 			continue
 		}
-
 		if err != nil {
 			files = append(files, v)
 		}
-
 	}
 	return nbytes, files
 }
 
 func fileSize(fi *os.File) int64 {
 	fil, err := fi.Stat()
-
 	if err != nil {
 		fmt.Println(err.Error())
 		return 0
 	}
-
 	return fil.Size()
 }
 
 func main() {
-
 	n := len(os.Args)
 	if n < 4 {
 		fmt.Println("Not enough arguments")
 		os.Exit(1)
 	}
-
 	nbytes, files := numberOfBytes(os.Args[1:])
-
 	printName := len(files) > 1
-
-	//open files for reading only
 	for j, f := range files {
 		fi, err := os.Open(f)
 		if err != nil {
@@ -71,22 +60,18 @@ func main() {
 		}
 		if printName {
 			fmt.Printf("==> %s <==\n", f)
-
 		}
 		read := make([]byte, int(nbytes))
 		_, er := fi.ReadAt(read, fileSize(fi)-int64(nbytes))
 		if er != nil {
 			fmt.Println(er.Error())
 		}
-
 		for _, c := range read {
 			z01.PrintRune(rune(c))
 		}
-
 		if j < len(files)-1 {
 			z01.PrintRune('\n')
 		}
-
 		fi.Close()
 	}
 }
