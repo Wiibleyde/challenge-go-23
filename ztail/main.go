d93411722e2311dbf73e3e3551c24892fe51a970
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-27 13:13:38 +0200

    ztail test

--- ztail/main.go
+++ ztail/main.go
@@ -3,34 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
-
-	"github.com/01-edu/z01"
 )
 
 func numberOfBytes(args []string) (int, []string) {
-	n := len(args)
+	// n := len(args)
 	nbytes := 0
 	var files []string
 	for i, v := range args {
-		var err error
-		_, err = strconv.Atoi(v)
-		if v == "-c" {
-			if i >= n-1 {
-				fmt.Printf("tail: option requires an argument -- 'c'\nTry 'tail --help' for more information.")
-				os.Exit(1)
-			}
-			arg := args[i+1]
-			nbytes, err = strconv.Atoi(arg)
-			if err != nil {
-				fmt.Printf("tail: invalid number of bytes: %s\n", arg)
-				os.Exit(1)
-			}
-			continue
-		}
-		if err != nil {
-			files = append(files, v)
-		}
+		arg := args[i+1]
+		nbytes = Atoi(arg)
+		_ = v
+		continue
 	}
 	return nbytes, files
 }
@@ -67,11 +50,67 @@ func main() {
 			fmt.Println(er.Error())
 		}
 		for _, c := range read {
-			z01.PrintRune(rune(c))
+			fmt.Print(c)
 		}
 		if j < len(files)-1 {
-			z01.PrintRune('\n')
+			fmt.Print("\n")
 		}
 		fi.Close()
 	}
+}
+
+func Atoi(s string) int {
+	if StrLen(s) == 0 {
+		return 0
+	}
+	s0 := s
+	if s[0] == '-' || s[0] == '+' {
+		s = s[1:]
+		if StrLen(s) < 1 {
+			return 0
+		}
+	}
+	nb := 0
+	for _, ch := range s {
+		if !containsIn0to9(ch) {
+			return 0
+		}
+		nb = nb*10 + charToInt(ch)
+	}
+	if s0[0] == '-' {
+		nb *= -1
+	}
+	return nb
+}
+
+func StrLen(s string) int {
+	letter := 0
+	for _, ele := range s {
+		letter++
+		_ = ele
+	}
+	return letter
+}
+
+func charToInt(char rune) int {
+	count := 0
+	if char < 48 && char > 58 {
+		return 0
+	}
+
+	for i := '1'; i <= char; i++ {
+		count++
+	}
+
+	return count
+}
+
+func containsIn0to9(ch rune) bool {
+	for i := '0'; i <= '9'; i++ {
+		if ch == i {
+			return true
+		}
+	}
+
+	return false
 }
