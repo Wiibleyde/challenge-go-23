23aa2761677167842e4f700eee562d085b8dab1b
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-28 14:11:43 +0200

    listpushfront

--- listpushback.go
+++ listpushback.go
@@ -1,5 +1,15 @@
 package student
 
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
 func ListPushBack(l *List, data interface{}) {
 	node := &NodeL{Data: data}
 	if l.Head == nil {
