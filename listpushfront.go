a7101428e50e71491e28e8086de8a15682bf3cc3
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-28 14:02:19 +0200

    listpushfront

---
+++ listpushfront.go
@@ -0,0 +1,22 @@
+package student
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	node := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = node
+		l.Tail = node
+	} else {
+		node.Next = l.Head
+		l.Head = node
+	}
+}
