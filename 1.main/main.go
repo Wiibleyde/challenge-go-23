670d572a317629bf2b976bf31b284b4d69867eb2
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-28 14:01:39 +0200

    listpushback

--- 1.main/main.go
+++ 1.main/main.go
@@ -6,13 +6,10 @@ import (
 )
 
 func main() {
-
 	link := &piscine.List{}
-
 	piscine.ListPushBack(link, "Hello")
 	piscine.ListPushBack(link, "man")
 	piscine.ListPushBack(link, "how are you")
-
 	for link.Head != nil {
 		fmt.Println(link.Head.Data)
 		link.Head = link.Head.Next
