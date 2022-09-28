b8e66a87b971b00927bb458182ee54612a308784
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-28 15:13:04 +0200

    listSort

--- 1.main/main.go
+++ 1.main/main.go
@@ -29,15 +29,11 @@ func listPushBack(l *piscine.NodeI, data int) *piscine.NodeI {
 }
 
 func main() {
-
 	var link *piscine.NodeI
-
 	link = listPushBack(link, 1)
 	link = listPushBack(link, 4)
 	link = listPushBack(link, 9)
-
 	PrintList(link)
-
 	link = piscine.SortListInsert(link, -2)
 	link = piscine.SortListInsert(link, 2)
 	PrintList(link)
