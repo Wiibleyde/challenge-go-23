027d1ca38ffe73fc9206aef4570f17ad3407b1cf
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-29 14:18:07 +0200

    binary tree

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,6 +1,6 @@
 package student
 
-func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error))  {
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
