71400371f98a61aae2e8696cf978c70c3d15b00d
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-29 15:35:31 +0200

    btree

--- btreedeletenode.go
+++ btreedeletenode.go
@@ -1,20 +1,6 @@
 package student
 
 func BTreeDeleteNode(root, node *TreeNode) *TreeNode {
-	if node.Left == nil {
-		return BTreeTransplant(root, node, node.Right)
-	} else if node.Right == nil {
-		return BTreeTransplant(root, node, node.Left)
-	} else {
-		min := BTreeMin(node.Right)
-		if min.Parent != node {
-			BTreeTransplant(root, min, min.Right)
-			min.Right = node.Right
-			min.Right.Parent = min
-		}
-		BTreeTransplant(root, node, min)
-		min.Left = node.Left
-		min.Left.Parent = min
-		return root
-	}
+	*node = *node.Parent
+	return root
 }
