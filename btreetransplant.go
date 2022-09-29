3902a235e1cd58ea2c95fd03eb882166ce8f7658
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-29 15:33:25 +0200

    btree

--- btreetransplant.go
+++ btreetransplant.go
@@ -1,19 +1,7 @@
 package student
 
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
-	if root == nil {
-		return nil
-	}
-
-	replacement := node
-	if node.Parent == nil {
-		root = rplc
-	} else if node == node.Parent.Left {
-		replacement.Parent.Left = rplc
-	} else {
-		replacement.Parent.Right = rplc
-	}
-	replacement.Parent = node.Parent
-
-	return root
+    rplc.Parent = node.Parent
+    *node = *rplc
+    return root
 }
